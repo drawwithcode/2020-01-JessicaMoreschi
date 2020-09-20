b1cd09b6a5b62508c823e916cfb0941e772dc9e6
JessicaMoreschi <71377205+JessicaMoreschi@users.noreply.github.com>
2020-09-20 11:54:36 +0200

    Proj01_20/10/20
    completo

--- sketch.js
+++ sketch.js
@@ -1,12 +1,91 @@
 function preload(){
-  // put preload code here
 }
+
+
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+//Canvas
+  createCanvas(windowWidth,windowHeight);
+  background('lightgrey');
+  angleMode(DEGREES);
+
+//Allert
+  push()
+    fill('white');
+    noStroke();
+    rectMode(CENTER);
+    rect(width/2, height/2, 500, 300, 10);
+  pop()
+
+  push()
+    fill('grey');
+    noStroke();
+    rectMode(CENTER);
+    rect(width/2, height/2-150, 500, 25, 10, 10, 0, 0);
+  pop()
+
+//Menu dots
+  push()
+    fill('red');
+    noStroke();
+    ellipseMode(CENTER);
+    ellipse(width/2-230, height/2-150, 10);
+  pop()
+
+  push()
+    fill('yellow');
+    noStroke();
+    ellipseMode(CENTER);
+    ellipse(width/2-210, height/2-150, 10);
+  pop()
+
+  push()
+    fill('lightgreen');
+    noStroke();
+    ellipseMode(CENTER);
+    ellipse(width/2-190, height/2-150, 10);
+  pop()
+
+//Text
+  push()
+    textAlign(CENTER);
+    textSize(15);
+    fill('grey');
+    text('buffering', width/2, height/2+85);
+  pop()
 }
 
+
+
 function draw() {
-  // put drawing code here
+//Circle
+  if(frameCount < 360/4) {
+    push()
+      strokeWeight(0);
+      stroke(lerpColor (color('red'), color('blue'), frameCount/360*4)); //color gradient
+      fill(lerpColor (color('red'), color('blue'), frameCount/360*4));
+
+      translate(width/2, height/2); //center allign
+      rotate(frameCount*4);
+      translate(30, 0);
+      arc(0, 0, 25, 25, 0, 180);
+    pop()
+  }
+
+//No circle
+  if(frameCount > 360/4) {
+      push()
+        strokeWeight(0.5);
+        stroke('white');
+        fill('white');
+
+        translate(width/2, height/2);
+        rotate(frameCount*4);
+        translate(30, 0);
+        arc(0, 0, 25, 25, 0, 180);
+
+        if(frameCount == 720/4) {frameCount=0}; //loop
+      pop()
+    }
+
 }
